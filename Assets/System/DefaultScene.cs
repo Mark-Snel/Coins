9e4db7b5d3609588b7cbe84f27bcf9b1c7e2fbd9
Mark-Snel <167076292+Mark-Snel@users.noreply.github.com>
2025-02-11 22:32:12 +0100

    inputmanager, samplescene

---
+++ Assets/System/DefaultScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentSystemsManager : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
